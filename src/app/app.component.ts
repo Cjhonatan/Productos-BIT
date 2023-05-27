86f23c3cab4221855401b9508bbcd6b1a07700bc
Cjhonatan <jhonnatan.catano@udea.edu.co>
2023-05-26 21:50:46 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Productos';
+  listaProductos = [
+    { nombre: 'Televisor', precio: 20000000 },
+    { nombre: 'Ventilador', precio: 500000 },
+    { nombre: 'Computador', precio: 5000000 },
+    { nombre: 'Celular', precio: 3500000 },
+    { nombre: 'Nevera', precio: 2200000}
+  ];
 }
